bdeb3909c0dd4b1b03815710cf0ed74bd0bf5d4f
Tungphamsoluis <Tung.Pham@soluis.com>
2026-03-06 05:25:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,51 @@
 const express = require("express")
 const WebSocket = require("ws")
+const http = require("http")
 
 const app = express()
 app.use(express.json())
 
-const server = app.listen(process.env.PORT || 3000)
+// route test
+app.get("/", (req,res)=>{
+    res.send("Webhook server running")
+})
+
+const server = http.createServer(app)
 
 const wss = new WebSocket.Server({ server })
 
 let clients = []
 
 wss.on("connection", ws => {
-    clients.push(ws)
+
     console.log("UE connected")
+    clients.push(ws)
+
+    ws.on("close", ()=>{
+        clients = clients.filter(c=>c!==ws)
+    })
 })
 
-app.post("/webhook", (req,res)=>{
+app.post("/webhook",(req,res)=>{
 
     const event = req.body
 
     if(event.event_type === "optimization_finished"){
 
-        const model = event.data.rapidmodel["rapid.glb"]
+        const url = event.data.rapidmodel["rapid.glb"]
 
         const msg = JSON.stringify({
             type:"model_ready",
-            url:model
+            url:url
         })
 
         clients.forEach(c=>c.send(msg))
     }
 
     res.sendStatus(200)
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, ()=>{
+    console.log("Server running on port",PORT)
 })
